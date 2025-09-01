850eb0c977222b9aee23f11a19c31b6cc22e145e
marcus88santos <mf8santos@gmail.com>
2025-09-01 16:41:26 -0300

    cleaning

--- android/app/src/main/kotlin/cinebox/marcus/cinebox_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package cinebox.marcus.cinebox_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:cinebox_app/cinebox_main_app.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(CineboxMainApp());
-}
