eddc42814188f7348d932436e4ab27c95f4be25b
marcus88santos <mf8santos@gmail.com>
2025-09-01 16:47:08 -0300

    cleaning

---
+++ lib/ui/splash/splash_screen.dart
@@ -0,0 +1,25 @@
+import 'package:cinebox_app/config/env.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class SplashScreen extends ConsumerStatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends ConsumerState<SplashScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Splash Screen'),
+      ),
+      body: Center(
+        child: Text(Env.backendBaseUrl),
+      ),
+    );
+  }
+}
