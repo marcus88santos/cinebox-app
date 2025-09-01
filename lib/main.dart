9a8cb46dc3069ad22cff9eaa3c729c52bfbef59c
marcus88santos <mf8santos@gmail.com>
2025-09-01 11:33:44 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:cinebox_app/cinebox_main_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CineboxMainApp());
+}
---
+++ lib/ui/splash/splash_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+fu-class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Splash Screen'),
+      ),
+      body: Container(
+        
+      ),
+    );
+  }
+}
